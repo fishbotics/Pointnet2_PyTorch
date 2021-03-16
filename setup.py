3aff1ddd7944e8624a9b252287fc723ceba6f2d4
Adam Fishman <afishman@cs.washington.edu>
2021-03-16 00:52:34 -0700

    Updated to use find namespace packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import os.path as osp
 
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 requirements = [
-    "hydra-core>=0.11.3", 
-    "pytorch-lightning>=0.7.1", 
+    "hydra-core>=0.11.3",
+    "pytorch-lightning>=0.7.1",
     "numpy>=1.20.1",
     "msgpack-numpy>=0.4.7.1",
     "lmdb>=1.1.1",
@@ -18,6 +18,6 @@ setup(
     name="pointnet2",
     version=__version__,
     author="Erik Wijmans",
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     install_requires=requirements,
 )
