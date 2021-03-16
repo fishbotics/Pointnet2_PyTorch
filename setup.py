2b068525898c3254f9f6300ca897a70d4d6d281e
Adam Fishman <45679966+fishbotics@users.noreply.github.com>
2021-03-15 22:24:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ import os.path as osp
 
 from setuptools import find_packages, setup
 
-requirements = ["hydra-core==0.11.3", "pytorch-lightning==0.7.1"]
+requirements = [
+    "hydra-core>=0.11.3", 
+    "pytorch-lightning>=0.7.1", 
+    "numpy>=1.20.1",
+    "msgpack-numpy>=0.4.7.1",
+    "lmdb>=1.1.1"
+    "h5py>=3.2.1",
+]
 
 
 exec(open(osp.join("pointnet2", "_version.py")).read())
